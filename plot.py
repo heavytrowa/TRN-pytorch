69def02ec532a6d6593ba7eb6f290ccb6987fb9a
js_fu <kevin83427@gmail.com>
2020-03-19 18:55:11 -0500

    plot

--- plot.py
+++ plot.py
@@ -1,8 +1,20 @@
+%matplotlib inline
 import json 
+import matplotlib.pyplot as plt
   
 # Opening JSON file 
 with open('timeline.json', 'r') as openfile: 
-  
     result = json.load(openfile) 
-  
-print(result["slipping"])
+
+time = []
+prob = []
+
+for pair in result["slipping"]:
+    time.append(pair[0])
+    prob.append(pair[1])
+
+plt.plot(time, prob, 'o-')
+plt.title('Timeline')
+plt.ylabel('Probability')
+plt.xlabel('time (sec.)')
+plt.show()
