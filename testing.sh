416accc1ec0ad7a9ec98be6900441829666dfe9e
heavytrowa <snipeslinger@gmail.com>
2020-03-23 18:02:13 -0500

    update the model to the one i trained

--- testing.sh
+++ testing.sh
@@ -1,1 +1,1 @@
-python test_segment.py --video_file sample_data/test2.mp4 --weight pretrain/TRN_moments_RGB_InceptionV3_TRNmultiscale_segment8_best.pth.tar --arch InceptionV3 --dataset moments
+python test_segment.py --video_file sample_data/test2.mp4 --weight model/TRN_moments_RGB_InceptionV3_TRNmultiscale_segment8_checkpoint.pth.tar --arch InceptionV3 --dataset moments
