2c188edc82b808a448b744574b3f9bbfd7d00db9
heavytrowa <snipeslinger@gmail.com>
2020-04-02 22:57:30 -0500

    fix typos

--- plot.py
+++ plot.py
@@ -1,12 +1,14 @@
+%matplotlib inline
 import json 
 import matplotlib.pyplot as plt
+  
+# Opening JSON file 
+with open('sample1.json', 'r') as openfile: 
+    result = json.load(openfile) 
 
-for i in range(1,6):
-    # Opening JSON file 
-    with open("sample"+str(i)+".json", 'r') as openfile: 
-        result = json.load(openfile) 
+time = []
+prob = []
 
-<<<<<<< HEAD
 for pair in result["crawling"]:
     time.append(pair[0])
     prob.append(pair[1])
@@ -16,17 +18,3 @@ plt.title('Timeline')
 plt.ylabel('Probability')
 plt.xlabel('time (sec.)')
 plt.show()
-=======
-    time = []
-    prob = []
-
-    for pair in result["crawling"]:
-        time.append(pair[0])
-        prob.append(pair[1])
-
-    plt.plot(time, prob, 'o-')
-    plt.title("Prediction Timeline for Sample "+str(i))
-    plt.ylabel('Probability')
-    plt.xlabel('time (sec.)')
-    plt.show()
->>>>>>> 2178cfdd68a5e2fb20deb310cae8e3a9f871bb2b
