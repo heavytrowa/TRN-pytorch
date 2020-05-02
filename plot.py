080e6118f1b1d0109987e5ea1abf76e3076095e6
heavytrowa <snipeslinger@gmail.com>
2020-05-01 23:48:55 -0500

    change it for the sh file

--- plot.py
+++ plot.py
@@ -1,20 +1,21 @@
-%matplotlib inline
 import json 
 import matplotlib.pyplot as plt
-  
+import sys
+
 # Opening JSON file 
-with open('timeLabel.json', 'r') as openfile: 
+with open(sys.argv[1], 'r') as openfile: 
     result = json.load(openfile) 
 
 time = []
 prob = []
 
-for pair in result["crawling"]:
+for pair in result["slipping"]:
     time.append(pair[0])
     prob.append(pair[1])
 
 plt.plot(time, prob, 'o-')
-plt.title('Timeline')
+plt.title("Prediction Timeline for Sample")
 plt.ylabel('Probability')
 plt.xlabel('time (sec.)')
-plt.show()
+plt.savefig(sys.argv[2])
+
